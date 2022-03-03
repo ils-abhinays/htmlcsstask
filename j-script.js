7f9f424399db6275f9334f0625555f65c927abc8
ils-abhinays <ab.sharma@thesynapses.com>
2022-03-03 17:31:56 +0530

    link imgage view dynamically

---
+++ j-script.js
@@ -0,0 +1,103 @@
+function test() 
+{
+    let elm = document.getElementById("img-a");
+    let s = elm.getAttribute("src");
+    localStorage.setItem("val-img", s);
+
+    let x = document.getElementById("heading-a");
+    let y = x.textContent;
+    localStorage.setItem("val", y);
+
+    return false;
+}
+
+function test2() 
+{
+    let elm = document.getElementById("img-b");
+    let s = elm.getAttribute("src");
+    localStorage.setItem("val-img", s);
+
+    let x = document.getElementById("heading-b");
+    let y = x.textContent;
+    localStorage.setItem("val", y);
+
+    return false;
+}
+
+function test3() 
+{
+    let elm = document.getElementById("img-c");
+    let s = elm.getAttribute("src");
+    localStorage.setItem("val-img", s);
+
+    let x = document.getElementById("heading-c");
+    let y = x.textContent;
+    localStorage.setItem("val", y);
+
+    return false;
+}
+
+function test4() 
+{
+    let elm = document.getElementById("img-d");
+    let s = elm.getAttribute("src");
+    localStorage.setItem("val-img", s);
+
+    let x = document.getElementById("heading-d");
+    let y = x.textContent;
+    localStorage.setItem("val", y);
+
+    return false;
+}
+
+function test5() 
+{
+    let elm = document.getElementById("img-e");
+    let s = elm.getAttribute("src");
+    localStorage.setItem("val-img", s);
+
+    let x = document.getElementById("heading-e");
+    let y = x.textContent;
+    localStorage.setItem("val", y);
+
+    return false;
+}
+
+function test6() 
+{
+    let elm = document.getElementById("img-f");
+    let s = elm.getAttribute("src");
+    localStorage.setItem("val-img", s);
+
+    let x = document.getElementById("heading-f");
+    let y = x.textContent;
+    localStorage.setItem("val", y);
+
+    return false;
+}
+
+function test7() 
+{
+    let elm = document.getElementById("img-g");
+    let s = elm.getAttribute("src");
+    localStorage.setItem("val-img", s);
+
+    let x = document.getElementById("heading-g");
+    let y = x.textContent;
+    localStorage.setItem("val", y);
+
+    return false;
+}
+
+function test8() 
+{
+    let elm = document.getElementById("img-h");
+    let s = elm.getAttribute("src");
+    localStorage.setItem("val-img", s);
+
+    let x = document.getElementById("heading-h");
+    let y = x.textContent;
+    localStorage.setItem("val", y);
+
+    return false;
+}
