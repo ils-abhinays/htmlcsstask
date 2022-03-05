bf0b039300ef8a0aeda9caa7e26165fbea2fddc8
ils-abhinays <ab.sharma@thesynapses.com>
2022-03-06 00:13:20 +0530

    make responsive

--- j-script.js
+++ j-script.js
@@ -1,11 +1,11 @@
 function test() 
 {
-    let elm = document.getElementById("img-a");
-    let s = elm.getAttribute("src");
+    var elm = document.getElementById("img-a");
+    var s = elm.getAttribute("src");
     localStorage.setItem("val-img", s);
 
-    let x = document.getElementById("heading-a");
-    let y = x.textContent;
+    var x = document.getElementById("heading-a");
+    var y = x.textContent;
     localStorage.setItem("val", y);
 
     return false;
@@ -13,12 +13,12 @@ function test()
 // --------------------------------------
 function test2() 
 {
-    let elm = document.getElementById("img-b");
-    let s = elm.getAttribute("src");
+    var elm = document.getElementById("img-b");
+    var s = elm.getAttribute("src");
     localStorage.setItem("val-img", s);
 
-    let x = document.getElementById("heading-b");
-    let y = x.textContent;
+    var x = document.getElementById("heading-b");
+    var y = x.textContent;
     localStorage.setItem("val", y);
 
     return false;
@@ -26,12 +26,12 @@ function test2()
 
 function test3() 
 {
-    let elm = document.getElementById("img-c");
-    let s = elm.getAttribute("src");
+    var elm = document.getElementById("img-c");
+    var s = elm.getAttribute("src");
     localStorage.setItem("val-img", s);
 
-    let x = document.getElementById("heading-c");
-    let y = x.textContent;
+    var x = document.getElementById("heading-c");
+    var y = x.textContent;
     localStorage.setItem("val", y);
 
     return false;
@@ -39,12 +39,12 @@ function test3()
 
 function test4() 
 {
-    let elm = document.getElementById("img-d");
-    let s = elm.getAttribute("src");
+    var elm = document.getElementById("img-d");
+    var s = elm.getAttribute("src");
     localStorage.setItem("val-img", s);
 
-    let x = document.getElementById("heading-d");
-    let y = x.textContent;
+    var x = document.getElementById("heading-d");
+    var y = x.textContent;
     localStorage.setItem("val", y);
 
     return false;
@@ -52,12 +52,12 @@ function test4()
 
 function test5() 
 {
-    let elm = document.getElementById("img-e");
-    let s = elm.getAttribute("src");
+    var elm = document.getElementById("img-e");
+    var s = elm.getAttribute("src");
     localStorage.setItem("val-img", s);
 
-    let x = document.getElementById("heading-e");
-    let y = x.textContent;
+    var x = document.getElementById("heading-e");
+    var y = x.textContent;
     localStorage.setItem("val", y);
 
     return false;
@@ -65,12 +65,12 @@ function test5()
 
 function test6() 
 {
-    let elm = document.getElementById("img-f");
-    let s = elm.getAttribute("src");
+    var elm = document.getElementById("img-f");
+    var s = elm.getAttribute("src");
     localStorage.setItem("val-img", s);
 
-    let x = document.getElementById("heading-f");
-    let y = x.textContent;
+    var x = document.getElementById("heading-f");
+    var y = x.textContent;
     localStorage.setItem("val", y);
 
     return false;
@@ -78,12 +78,12 @@ function test6()
 
 function test7() 
 {
-    let elm = document.getElementById("img-g");
-    let s = elm.getAttribute("src");
+    var elm = document.getElementById("img-g");
+    var s = elm.getAttribute("src");
     localStorage.setItem("val-img", s);
 
-    let x = document.getElementById("heading-g");
-    let y = x.textContent;
+    var x = document.getElementById("heading-g");
+    var y = x.textContent;
     localStorage.setItem("val", y);
 
     return false;
@@ -91,17 +91,13 @@ function test7()
 
 function test8() 
 {
-    let elm = document.getElementById("img-h");
-    let s = elm.getAttribute("src");
+    var elm = document.getElementById("img-h");
+    var s = elm.getAttribute("src");
     localStorage.setItem("val-img", s);
 
-    let x = document.getElementById("heading-h");
-    let y = x.textContent;
-<<<<<<< HEAD
-    localStorage.setItem("val", y); 
-=======
+    var x = document.getElementById("heading-h");
+    var y = x.textContent;
     localStorage.setItem("val", y);
->>>>>>> 7f9f424399db6275f9334f0625555f65c927abc8
-
+    localStorage.setItem("val", y);
     return false;
 }
