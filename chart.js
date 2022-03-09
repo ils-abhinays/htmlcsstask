5adc2514e7692957b084e4334e356fea3c8cf9f9
ils-abhinays <ab.sharma@thesynapses.com>
2022-03-09 22:04:25 +0530

    add charts

--- chart.js
+++ chart.js
@@ -1,28 +1,53 @@
 var crt = document.getElementById("piechart").getContext('2d');
 new Chart(crt, {
     type: "pie",
-    data: 
+    data:
     {
         labels: ['Party hub pass', 'Disco dancer', 'Sparkling Nights'],
-        datasets: 
-        [{
-            backgroundColor:
-                [
-                    'rgba(252, 3, 3)',
-                    'rgba(3, 157, 252)',
-                    'rgba(0, 255, 0)'
-                ],
-            data: [591, 480, 412]
-        }]
+        datasets:
+            [{
+                backgroundColor:
+                    [
+                        'rgba(252, 3, 3)',
+                        'rgba(3, 157, 252)',
+                        'rgba(0, 255, 0)'
+                    ],
+                data: [591, 480, 412],
+            }]
     },
-    options: {
-        title: {
+    options: 
+    {
+        title:
+        {
             display: true,
-            text: "World Wide Wine Production"
+            text: "Top 3 events per View",
+            fontSize: 25
+        },
+        plugins: 
+        {
+            datalabels: {
+                display:true,
+                formatter: (value) => {
+                  return value + ' views';
+                // return crt.chart.data[crt.dataIndex]
+            },
+            color: 'white',
+              }
+        
+        },
+            legend: 
+            {
+                display: true,
+                position: 'bottom',
+                align: 'end',
+                labels: 
+                {
+                    fontSize: 10,
+                    boxWidth: 10,
+                }
+            }
         }
-    }
-});
-
+    });
 // ---------------------1---------------------------------
 
 var crt1 = document.getElementById("linechart").getContext('2d');
@@ -34,25 +59,57 @@ var myChart = new Chart(crt1,
             labels: ['jan', 'feb', 'mar', 'apr'],
             datasets:
                 [{
+                    // label: 'All download',
                     data: [260, 320, 150, 500],
                     fill: false,
                     pointRadius: 5,
-                    borderColor: "rgba(255,0,0)",
+                    pointBackgroundColor: "rgb(226, 21, 182)",
+                    borderColor: "rgb(226, 21, 182)",
                 }],
+
         },
-        options: {
-            responsive: false,
-            title: {
-                display: true,
-                text: "All Downloads"
+        options: 
+        {
+            layout:{
+                padding: 50,
+            },
+            responsive: true,
+            scales: 
+            {
+                xAxes: 
+                [{
+                    gridLines: {
+                        display: false }
+                }],
+                yAxes: 
+                [{
+                    display: false,
+                    gridLines: {
+                    display: false
+                    }
+                }]
+            },
+            legend: 
+            {
+              display: false,
+            },
+            plugins: 
+            {
+            datalabels:  
+            {
+            color: "rgb(226, 21, 182)",
+            display:true+'k',
+            anchor: "end",
+            align:"end"
             }
         }
+    }
     });
 
 // ---------------------2---------------------------------
 
-var crt1 = document.getElementById("linechart2").getContext('2d');
-var myChart = new Chart(crt1,
+var crt2 = document.getElementById("linechart2").getContext('2d');
+var myChart = new Chart(crt2,
     {
         type: "line",
         data:
@@ -63,21 +120,51 @@ var myChart = new Chart(crt1,
                     data: [76, 59, 75, 120],
                     fill: false,
                     pointRadius: 5,
-                    borderColor: "rgba(255,0,0)",
+                    pointBackgroundColor: "rgb(226, 21, 182)",
+                    borderColor: "rgb(226, 21, 182)",
                 }],
         },
-        options: {
-            responsive: false,
-            title: {
-                display: true,
-                text: "All Downloads"
+        options: 
+        {
+            layout:{
+                padding: 50,
+            },
+            responsive: true,
+            scales: 
+            {
+                xAxes: 
+                [{
+                    gridLines: {
+                        display: false }
+                }],
+                yAxes: 
+                [{
+                    display: false,
+                    gridLines: {
+                    display: false
+                    }
+                }]
+            },
+            legend: 
+            {
+              display: false,
+            },
+            plugins: 
+            {
+            datalabels:  
+            {
+            color: "rgb(226, 21, 182)",
+            display:true+'k',
+            anchor: "end",
+            align:"end"
             }
         }
+    }
     });
 // ------------------------3------------------------------
 
-var crt1 = document.getElementById("linechart3").getContext('2d');
-var myChart = new Chart(crt1,
+var crt3 = document.getElementById("linechart3").getContext('2d');
+var myChart = new Chart(crt3,
     {
         type: "line",
         data:
@@ -88,21 +175,51 @@ var myChart = new Chart(crt1,
                     data: [920, 2000, 1200, 1000],
                     fill: false,
                     pointRadius: 5,
-                    borderColor: "rgba(255,0,0)",
+                    pointBackgroundColor: "rgb(226, 21, 182)",
+                    borderColor: "rgb(226, 21, 182)",
                 }],
         },
-        options: {
-            responsive: false,
-            title: {
-                display: true,
-                text: "All Downloads"
+        options: 
+        {
+            layout:{
+                padding: 50,
+            },
+            responsive: true,
+            scales: 
+            {
+                xAxes: 
+                [{
+                    gridLines: {
+                        display: false }
+                }],
+                yAxes: 
+                [{
+                    display: false,
+                    gridLines: {
+                    display: false
+                    }
+                }]
+            },
+            legend: 
+            {
+              display: false,
+            },
+            plugins: 
+            {
+            datalabels:  
+            {
+            color: "rgb(226, 21, 182)",
+            display:true+'k',
+            anchor: "end",
+            align:"end"
             }
         }
+    }
     });
 // ------------------------4------------------------------
 
-var crt1 = document.getElementById("linechart4").getContext('2d');
-var myChart = new Chart(crt1,
+var crt4 = document.getElementById("linechart4").getContext('2d');
+var myChart = new Chart(crt4,
     {
         type: "line",
         data:
@@ -113,14 +230,45 @@ var myChart = new Chart(crt1,
                     data: [920, 2000, 1200, 1000],
                     fill: false,
                     pointRadius: 5,
-                    borderColor: "rgba(255,0,0)",
+                    pointBackgroundColor: "rgb(226, 21, 182)",
+                    borderColor: "rgb(226, 21, 182)",
                 }],
         },
-        options: {
-            responsive: false,
-            title: {
-                display: true,
-                text: "All Downloads"
+        options: 
+        {
+            maintainAspectRatio: true,
+            layout:{
+                padding: 50,
+            },
+            responsive: true,
+            scales: 
+            {
+                xAxes: 
+                [{
+                    gridLines: {
+                        display: false }
+                }],
+                yAxes: 
+                [{
+                    display: false,
+                    gridLines: {
+                    display: false
+                    }
+                }]
+            },
+            legend: 
+            {
+              display: false,
+            },
+            plugins: 
+            {
+            datalabels:  
+            {
+            color: "rgb(226, 21, 182)",
+            display:true+'k',
+            anchor: "end",
+            align:"end"
             }
         }
+    }
     });
